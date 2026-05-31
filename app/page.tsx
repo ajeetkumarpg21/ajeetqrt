da6cc0dd885702419e0754c24e525c8adb870fdb
ajeetkumarpg21 <ajeet.kumar.pg21@nsut.ac.in>
2026-05-31 09:45:06 +0530

    Initial Next.js project setup with TypeScript and dependencies

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import QuickStats from '@/components/QuickStats'
+import CTA from '@/components/CTA'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <QuickStats />
+      <About />
+      <CTA />
+    </>
+  )
+}
